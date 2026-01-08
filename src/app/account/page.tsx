5d885dbe5d18cb92c2e9b0dd9d4894ef055ff146
gampul <movetomvie@gmail.com>
2026-01-08 12:42:23 +0900

    remove: ?ъ옄?꾪솴 ?쒕ぉ 移대뱶 ??젣 (?ъ옄?쇨린 湲곕뒫? ?좎?)

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -768,13 +768,6 @@ export default function AccountPage() {
       
         <div className="max-w-[412px] mx-auto px-4 sm:px-6 py-4 sm:py-6">
         <div className="space-y-2">
-          {/* 제목 */}
-          <div className="bg-[rgb(254,252,247)] dark:bg-gray-800 rounded-xl sm:rounded-2xl border border-gray-200 dark:border-gray-700 p-4 sm:p-5 mb-2">
-            <h1 className="text-lg sm:text-xl font-bold text-gray-900 dark:text-white">
-              💰 투자현황
-            </h1>
-          </div>
-
           {/* 투자일기 */}
           <InvestmentDiary />
 
